5ac737222e342df11ce147f24e13d9df1d1caa67
Egor <kamolikov.en@gmail.com>
2025-03-29 01:55:05 +0300

    Добавил в audio_repository: сохранение метаданных загружаемого аудиофайла

--- app/repositories/audio_repository.py
+++ app/repositories/audio_repository.py
@@ -9,3 +9,20 @@ class AudioRepository:
         result = await db.execute(select(AudioFile).where(AudioFile.user_id == user_id))
 
         return result.scalars().all()
+
+    @staticmethod
+    async def create_audio_file(
+        user_id: int,
+        filename: str,
+        path: str,
+        db: AsyncSession
+    ) -> AudioFile:
+        audio = AudioFile(
+            filename=filename,
+            filepath=path,
+            user_id=user_id
+        )
+        db.add(audio)
+        await db.commit()
+        await db.refresh(audio)
+        return audio
