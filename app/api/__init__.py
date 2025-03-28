5c62e0f397fc4f7d15827742dc259317a4fff8cc
Egor <kamolikov.en@gmail.com>
2025-03-29 01:24:58 +0300

    Подключил audio_routes к общему роутеру

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
 from app.api.auth_routes import router as core_router
+from app.api.audio_routes import router as audio_router
 
 router = APIRouter()
+
+router.include_router(audio_router)
 router.include_router(core_router)
